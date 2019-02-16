78f2d82e012e7779daa5d3973545655341a07eee
Vincent Storme <me@vincentstorme.com>
2019-02-15 19:52:03 -0600

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.hypaer/ant-man "0.1.4"
+(defproject com.hypaer/ant-man "0.2.0"
   :description "Simple wrapper for Ant Design React components"
   :url "https://github.com/hypaer/ant-man.git"
   :license {:name "Eclipse Public License"
